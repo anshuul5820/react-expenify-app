eb2b3178d83c832eb783591470aacbc520126359
Schuyler Dare <schuylerdare96@yahoo.com>
2020-08-07 21:10:28 +0530

    modified webpack.config

--- webpack.config.js
+++ webpack.config.js
@@ -2,11 +2,11 @@ const path = require("path");
 const webpack = require("webpack");
 const ExtractTextPlugin = require("extract-text-webpack-plugin");
 
-process.ENV.NODE_ENV = process.ENV.NODE_ENV || "development";
+process.env.NODE_ENV = process.env.NODE_ENV || "development";
 
-if (process.ENV.NODE_ENV === "test") {
+if (process.env.NODE_ENV === "test") {
   require("dotenv").config({ path: ".env.test" });
-} else if (process.NODE_ENV === "development") {
+} else if (process.env.NODE_ENV === "development") {
   require("dotenv").config({ path: ".env.development" });
 }
 module.exports = (env) => {
