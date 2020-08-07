16532ee241637288243dbe74132c8f10e6ee7d0c
Schuyler Dare <schuylerdare96@yahoo.com>
2020-08-07 21:01:35 +0530

    setup test database

--- src/test/reducers/filters.test.js
+++ src/test/reducers/filters.test.js
@@ -1,6 +1,37 @@
 import filtersReducer from "../../reducers/filters";
-
+import moment from "moment";
 test("SHould setup default filter values", () => {
   const state = filtersReducer(undefined, { type: "@@INIT" });
-  expect(state).toBe()
+  expect(state).toBe();
+});
+
+test("should set sortBYy to amount", () => {
+  const state = filtersReducer(undefined, { type: "SORT_BY_AMOUNT" });
+  expect(state.sortBy).toBe("amount");
+});
+
+test("SHould set sortBy to date", () => {
+  const currentState = {
+    text: "",
+    startDate: undefined,
+    endDate: undefined,
+    sortBy: "amount",
+  };
+  const action = { type: "SORT_BY_DATE" };
+  const state = filtersReducer(currentState, action);
+  expect(state.sortBy).toBe("date");
+});
+
+test("Should set textfilter", () => {
+  const text = "this is the filter";
+  const action = { type: "SET_TEXT_FILTER", text };
+  const state = filtersReducer(undefined, action);
+  expect(state.text).toBe(text);
+});
+
+test("Should set startDate filter", () => {
+  const startDate = moment();
+  const action = { type: "SET_END_DATE", endDate };
+  const state = filtersReducer(undefined, action);
+  expect(state.endDate).toEqual(endDate);
 });
--- webpack.config.js
+++ webpack.config.js
@@ -1,49 +1,83 @@
-const path = require('path');
-const ExtractTextPlugin = require('extract-text-webpack-plugin');
+const path = require("path");
+const webpack = require("webpack");
+const ExtractTextPlugin = require("extract-text-webpack-plugin");
 
+process.ENV.NODE_ENV = process.ENV.NODE_ENV || "development";
+
+if (process.ENV.NODE_ENV === "test") {
+  require("dotenv").config({ path: ".env.test" });
+} else if (process.NODE_ENV === "development") {
+  require("dotenv").config({ path: ".env.development" });
+}
 module.exports = (env) => {
-  const isProduction = env === 'production';
-  const CSSExtract = new ExtractTextPlugin('styles.css');
+  const isProduction = env === "production";
+  const CSSExtract = new ExtractTextPlugin("styles.css");
 
   return {
-    entry: './src/app.js',
+    entry: "./src/app.js",
     output: {
-      path: path.join(__dirname, 'public', 'dist'),
-      filename: 'bundle.js'
+      path: path.join(__dirname, "public", "dist"),
+      filename: "bundle.js",
     },
     module: {
-      rules: [{
-        loader: 'babel-loader',
-        test: /\.js$/,
-        exclude: /node_modules/
-      }, {
-        test: /\.s?css$/,
-        use: CSSExtract.extract({
-          use: [
-            {
-              loader: 'css-loader',
-              options: {
-                sourceMap: true
-              }
-            },
-            {
-              loader: 'sass-loader',
-              options: {
-                sourceMap: true
-              }
-            }
-          ]
-        })
-      }]
+      rules: [
+        {
+          loader: "babel-loader",
+          test: /\.js$/,
+          exclude: /node_modules/,
+        },
+        {
+          test: /\.s?css$/,
+          use: CSSExtract.extract({
+            use: [
+              {
+                loader: "css-loader",
+                options: {
+                  sourceMap: true,
+                },
+              },
+              {
+                loader: "sass-loader",
+                options: {
+                  sourceMap: true,
+                },
+              },
+            ],
+          }),
+        },
+      ],
     },
     plugins: [
-      CSSExtract
+      CSSExtract,
+      new webpack.DefinePlugin({
+        "process.env.FIREBASE_API_KEY": JSON.stringify(
+          process.ENV.FIREBASE_API_KEY
+        ),
+        "process.env.FIREBASE_AUTH_DOMAIN": JSON.stringify(
+          process.ENV.FIREBASE_AUTH_DOMAIN
+        ),
+        "process.env.FIREBASE_DATABASE_URL": JSON.stringify(
+          process.ENV.FIREBASE_DATABASE_URL
+        ),
+        "process.env.FIREBASE_PROJECT_ID": JSON.stringify(
+          process.ENV.FIREBASE_PROJECT_ID
+        ),
+        "process.env.FIREBASE_STOTRAGE_BUCKEY": JSON.stringify(
+          process.ENV.FIREBASE_STOTRAGE_BUCKEY
+        ),
+        "process.env.FIREBASE_MESSAGING_SENDER_ID": JSON.stringify(
+          process.FIREBASE_MESSAGING_SENDER_ID
+        ),
+        "process.env.FIREBASE_APP_ID": JSON.stringify(
+          process.env.FIREBASE_APP_ID
+        ),
+      }),
     ],
-    devtool: isProduction ? 'source-map' : 'inline-source-map',
+    devtool: isProduction ? "source-map" : "inline-source-map",
     devServer: {
-      contentBase: path.join(__dirname, 'public'),
+      contentBase: path.join(__dirname, "public"),
       historyApiFallback: true,
-      publicPath: '/dist/'
-    }
+      publicPath: "/dist/",
+    },
   };
 };
